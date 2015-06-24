094182a344e2286bd13d752cc561b99b7275056b
Philipp Singer <philipp.singer@gesis.org>
2015-06-24 13:16:43 +0200

    new version

---
+++ setup.py
@@ -0,0 +1,14 @@
+__author__ = 'singerpp'
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='hyptrails',
+    version='0.3',
+    author='Philipp Singer',
+    author_email='philipp.singer@gesis.org',
+    packages=['hyptrails'],
+    license='MIT License',
+    url='https://github.com/psinger/HypTrails',
+)
