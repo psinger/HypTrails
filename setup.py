749d2fc2eb71052591b67a7bae671a8371c0737b
unknown <singerpp@KOL15028.gesis.intra>
2015-08-03 12:36:22 +0200

    minor

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import find_packages
 
 setup(
     name='hyptrails',
-    version='0.3',
+    version='0.4',
     author='Philipp Singer',
     author_email='philipp.singer@gesis.org',
     packages=['hyptrails'],
     license='MIT License',
     url='https://github.com/psinger/HypTrails',
     install_requires=[
-      'pathtools==0.5',
+      'pathtools==0.6',
     ],
     dependency_links=[
       'https://github.com/psinger/PathTools/archive/master.zip#egg=pathtools-0.5'
